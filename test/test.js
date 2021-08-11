8fb1c77cde69eeea880206764d87de5dec8ebde9
xingkailun <1813408689@qq.com>
2021-08-11 13:03:53 +0800

    优化测试

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-let excelconvert = require('../excelconvert/excelconvert');
-
+let excelconvert = //require('../dist/excelconvert.min')
+    //require('../excelconvert/excelconvert');
+    require('../dist/excelconvert')
 let fs = require('fs');
 let path = require('path');
-let url = path.join(__dirname + '/test.xlsx');
+let url = path.join(__dirname + '/test3.xlsx');
 let buffer = fs.readFileSync(url);
 
 let json = excelconvert.convert(new Uint8Array(buffer))
-console.log(require('../dist/excelconvert'));
+console.log(json);
