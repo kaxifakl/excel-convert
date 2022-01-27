5876d2a4dc9a8a6e64a1d41af585321a4e190956
kaxifa <1813408689@qq.com>
2022-01-27 15:16:49 +0800

    更新dts

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,15 @@
-export module excelconvert {
-    export let XLSX: any;
+export let XLSX: any;
 
-    export let convert: (data, fileName) => any;
+export let convert: (data, fileName) => any;
 
-    export let convertToTs: (fileName, jsonObj) => string;
+export let convertToTs: (fileName, jsonObj) => string;
 
-    export let setDefaultTypeValue: (type, value) => void;
+export let setDefaultTypeValue: (type, value) => void;
 
-    export let getDefaultTypeValue: (type) => void;
+export let getDefaultTypeValue: (type) => void;
 
-    export let addCustomTypeParse: (type, func) => void;
+export let addCustomTypeParse: (type, func) => void;
 
-    export let setCustomConvert: (fuc) => void;
+export let setCustomConvert: (fuc) => void;
 
-    export let arrayToObject: (dataArray, key) => any;
-} 
+export let arrayToObject: (dataArray, key) => any;
